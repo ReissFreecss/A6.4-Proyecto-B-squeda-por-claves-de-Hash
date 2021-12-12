3b71d9cc65a2f47a2de6dbcce37344a191cedc79
carlos <carlos@DESKTOP-D73TIV5>
2021-12-12 13:07:41 -0600

    Primer commit del proyecto

---
+++ src/mdbusqueda/MdBusqueda.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mdbusqueda;
+
+/**
+ *
+ * @author carlos
+ */
+public class MdBusqueda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
